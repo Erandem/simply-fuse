e0c273ae3469429eadf1f13b5daa192ea9e8c820
Josh Kallas <jkallas@protonmail.com>
2022-01-05 13:37:59 -0800

    Move Attribute structures to `src/attrs.rs`

---
+++ src/attrs.rs
@@ -0,0 +1,219 @@
+use std::time::Duration;
+
+use typed_builder::TypedBuilder;
+
+#[derive(Copy, Clone, Debug, TypedBuilder)]
+#[builder(field_defaults(default, setter(into)))]
+pub struct FileAttributes {
+    #[builder(!default, setter(!strip_option))]
+    mode: u32,
+    #[builder(default = 4096, setter(!strip_option))]
+    size: u64,
+    nlink: u32,
+
+    uid: u32,
+    gid: u32,
+
+    rdev: u32,
+    blksize: u32,
+    blocks: u64,
+
+    atime: Duration,
+    mtime: Duration,
+    ctime: Duration,
+
+    #[builder(default = Duration::from_secs(1))]
+    ttl: Duration,
+}
+
+impl FileAttributes {
+    pub fn mode(&self) -> u32 {
+        self.mode
+    }
+
+    pub fn size(&self) -> u64 {
+        self.size
+    }
+
+    pub fn nlink(&self) -> u32 {
+        self.nlink
+    }
+
+    pub fn uid(&self) -> u32 {
+        self.uid
+    }
+
+    pub fn gid(&self) -> u32 {
+        self.gid
+    }
+
+    pub fn rdev(&self) -> u32 {
+        self.rdev
+    }
+
+    pub fn blksize(&self) -> u32 {
+        self.blksize
+    }
+
+    pub fn blocks(&self) -> u64 {
+        self.blocks
+    }
+
+    pub fn atime(&self) -> Duration {
+        self.atime
+    }
+
+    pub fn mtime(&self) -> Duration {
+        self.mtime
+    }
+
+    pub fn ctime(&self) -> Duration {
+        self.ctime
+    }
+
+    /// # Note
+    /// `ttl` means `time to live`
+    /// This is **not** time to live, such as you'd go live on Twitch. It means time to live,
+    /// as in the remaining time you have left alive. I spent way too long misunderstanding
+    /// that.
+    pub fn ttl(&self) -> Duration {
+        self.ttl
+    }
+
+    pub fn set_mode(mut self, mode: u32) -> FileAttributes {
+        self.mode = mode;
+        self
+    }
+
+    pub fn set_size(mut self, size: u64) -> FileAttributes {
+        self.size = size;
+        self
+    }
+
+    pub fn set_nlink(mut self, nlink: u32) -> FileAttributes {
+        self.nlink = nlink;
+        self
+    }
+
+    pub fn set_uid(mut self, uid: u32) -> FileAttributes {
+        self.uid = uid;
+        self
+    }
+
+    pub fn set_gid(mut self, gid: u32) -> FileAttributes {
+        self.gid = gid;
+        self
+    }
+
+    pub fn set_rdev(mut self, rdev: u32) -> FileAttributes {
+        self.rdev = rdev;
+        self
+    }
+
+    pub fn set_blksize(mut self, blksize: u32) -> FileAttributes {
+        self.blksize = blksize;
+        self
+    }
+
+    pub fn set_blocks(mut self, blocks: u64) -> FileAttributes {
+        self.blocks = blocks;
+        self
+    }
+
+    pub fn set_atime(mut self, atime: Duration) -> FileAttributes {
+        self.atime = atime;
+        self
+    }
+
+    pub fn set_mtime(mut self, mtime: Duration) -> FileAttributes {
+        self.mtime = mtime;
+        self
+    }
+
+    pub fn set_ctime(mut self, ctime: Duration) -> FileAttributes {
+        self.ctime = ctime;
+        self
+    }
+
+    pub fn set_ttl(mut self, ttl: Duration) -> FileAttributes {
+        self.ttl = ttl;
+        self
+    }
+
+    #[deny(unused_variables)]
+    pub fn apply_attrs(&mut self, attrs: SetFileAttributes) -> FileAttributes {
+        // Here's a cool trick: By denying unused variables for this function and unpacking the
+        // struct below, this function will fail to compile if we update SetFileAttributes without
+        // modifying this function. Sure, there's reasons we might want to do that in the future,
+        // but we also want to make sure we always modify these variables
+        let SetFileAttributes {
+            mode,
+            size,
+            uid,
+            gid,
+            atime,
+            mtime,
+            ctime,
+        } = attrs;
+
+        // TODO convert this to macro_rules! maybe
+        macro copy_attr($name:ident) {
+            if let Some(attr) = $name {
+                self.$name = attr;
+            }
+        }
+
+        copy_attr!(mode);
+        copy_attr!(size);
+        copy_attr!(uid);
+        copy_attr!(gid);
+        copy_attr!(atime);
+        copy_attr!(mtime);
+        copy_attr!(ctime);
+
+        *self
+    }
+}
+
+#[derive(Copy, Clone, Debug, TypedBuilder)]
+pub struct SetFileAttributes {
+    mode: Option<u32>,
+    size: Option<u64>,
+
+    uid: Option<u32>,
+    gid: Option<u32>,
+
+    atime: Option<Duration>,
+    mtime: Option<Duration>,
+    ctime: Option<Duration>,
+}
+
+impl SetFileAttributes {
+    pub fn mode(&self) -> Option<u32> {
+        self.mode
+    }
+
+    pub fn size(&self) -> Option<u64> {
+        self.size
+    }
+
+    pub fn uid(&self) -> Option<u32> {
+        self.uid
+    }
+
+    pub fn gid(&self) -> Option<u32> {
+        self.gid
+    }
+
+    pub fn atime(&self) -> Option<Duration> {
+        self.atime
+    }
+
+    pub fn mtime(&self) -> Option<Duration> {
+        self.mtime
+    }
+
+    pub fn ctime(&self) -> Option<Duration> {
+        self.ctime
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![feature(decl_macro)]
+pub mod attrs;
 pub mod basic;
 pub mod error;
 
+use crate::attrs::*;
 use crate::error::{FSError, FSResult, PolyfuseError, Result};
 
 use std::ffi::{OsStr, OsString};
@@ -29,222 +31,6 @@ impl INode {
 impl From<u64> for INode {
     fn from(i: u64) -> INode {
         INode(i)
-    }
-}
-
-#[derive(Copy, Clone, Debug, TypedBuilder)]
-#[builder(field_defaults(default, setter(into)))]
-pub struct FileAttributes {
-    #[builder(!default, setter(!strip_option))]
-    mode: u32,
-    #[builder(default = 4096, setter(!strip_option))]
-    size: u64,
-    nlink: u32,
-
-    uid: u32,
-    gid: u32,
-
-    rdev: u32,
-    blksize: u32,
-    blocks: u64,
-
-    atime: Duration,
-    mtime: Duration,
-    ctime: Duration,
-
-    #[builder(default = Duration::from_secs(1))]
-    ttl: Duration,
-}
-
-impl FileAttributes {
-    pub fn mode(&self) -> u32 {
-        self.mode
-    }
-
-    pub fn size(&self) -> u64 {
-        self.size
-    }
-
-    pub fn nlink(&self) -> u32 {
-        self.nlink
-    }
-
-    pub fn uid(&self) -> u32 {
-        self.uid
-    }
-
-    pub fn gid(&self) -> u32 {
-        self.gid
-    }
-
-    pub fn rdev(&self) -> u32 {
-        self.rdev
-    }
-
-    pub fn blksize(&self) -> u32 {
-        self.blksize
-    }
-
-    pub fn blocks(&self) -> u64 {
-        self.blocks
-    }
-
-    pub fn atime(&self) -> Duration {
-        self.atime
-    }
-
-    pub fn mtime(&self) -> Duration {
-        self.mtime
-    }
-
-    pub fn ctime(&self) -> Duration {
-        self.ctime
-    }
-
-    /// # Note
-    /// `ttl` means `time to live`
-    /// This is **not** time to live, such as you'd go live on Twitch. It means time to live,
-    /// as in the remaining time you have left alive. I spent way too long misunderstanding
-    /// that.
-    pub fn ttl(&self) -> Duration {
-        self.ttl
-    }
-
-    pub fn set_mode(mut self, mode: u32) -> FileAttributes {
-        self.mode = mode;
-        self
-    }
-
-    pub fn set_size(mut self, size: u64) -> FileAttributes {
-        self.size = size;
-        self
-    }
-
-    pub fn set_nlink(mut self, nlink: u32) -> FileAttributes {
-        self.nlink = nlink;
-        self
-    }
-
-    pub fn set_uid(mut self, uid: u32) -> FileAttributes {
-        self.uid = uid;
-        self
-    }
-
-    pub fn set_gid(mut self, gid: u32) -> FileAttributes {
-        self.gid = gid;
-        self
-    }
-
-    pub fn set_rdev(mut self, rdev: u32) -> FileAttributes {
-        self.rdev = rdev;
-        self
-    }
-
-    pub fn set_blksize(mut self, blksize: u32) -> FileAttributes {
-        self.blksize = blksize;
-        self
-    }
-
-    pub fn set_blocks(mut self, blocks: u64) -> FileAttributes {
-        self.blocks = blocks;
-        self
-    }
-
-    pub fn set_atime(mut self, atime: Duration) -> FileAttributes {
-        self.atime = atime;
-        self
-    }
-
-    pub fn set_mtime(mut self, mtime: Duration) -> FileAttributes {
-        self.mtime = mtime;
-        self
-    }
-
-    pub fn set_ctime(mut self, ctime: Duration) -> FileAttributes {
-        self.ctime = ctime;
-        self
-    }
-
-    pub fn set_ttl(mut self, ttl: Duration) -> FileAttributes {
-        self.ttl = ttl;
-        self
-    }
-
-    #[deny(unused_variables)]
-    pub fn apply_attrs(&mut self, attrs: SetFileAttributes) -> FileAttributes {
-        // Here's a cool trick: By denying unused variables for this function and unpacking the
-        // struct below, this function will fail to compile if we update SetFileAttributes without
-        // modifying this function. Sure, there's reasons we might want to do that in the future,
-        // but we also want to make sure we always modify these variables
-        let SetFileAttributes {
-            mode,
-            size,
-            uid,
-            gid,
-            atime,
-            mtime,
-            ctime,
-        } = attrs;
-
-        // TODO convert this to macro_rules! maybe
-        macro copy_attr($name:ident) {
-            if let Some(attr) = $name {
-                self.$name = attr;
-            }
-        }
-
-        copy_attr!(mode);
-        copy_attr!(size);
-        copy_attr!(uid);
-        copy_attr!(gid);
-        copy_attr!(atime);
-        copy_attr!(mtime);
-        copy_attr!(ctime);
-
-        *self
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub struct SetFileAttributes {
-    mode: Option<u32>,
-    size: Option<u64>,
-
-    uid: Option<u32>,
-    gid: Option<u32>,
-
-    atime: Option<Duration>,
-    mtime: Option<Duration>,
-    ctime: Option<Duration>,
-}
-
-impl SetFileAttributes {
-    pub fn mode(&self) -> Option<u32> {
-        self.mode
-    }
-
-    pub fn size(&self) -> Option<u64> {
-        self.size
-    }
-
-    pub fn uid(&self) -> Option<u32> {
-        self.uid
-    }
-
-    pub fn gid(&self) -> Option<u32> {
-        self.gid
-    }
-
-    pub fn atime(&self) -> Option<Duration> {
-        self.atime
-    }
-
-    pub fn mtime(&self) -> Option<Duration> {
-        self.mtime
-    }
-
-    pub fn ctime(&self) -> Option<Duration> {
-        self.ctime
     }
 }
 
@@ -411,7 +197,7 @@ impl<T: Filesystem> Runner<T> {
             Ok(obj) => {
                 let mut conv: reply::AttrOut = reply::AttrOut::default();
                 conv.attr().ino(op.ino()); // FileAttribute does not keep the inode
-                conv.ttl(obj.ttl);
+                conv.ttl(obj.ttl());
 
                 self.copy_file_attr(&obj, op.ino().into(), conv.attr());
                 req.reply(conv).map_err(PolyfuseError::ReplyError)?;
@@ -442,23 +228,21 @@ impl<T: Filesystem> Runner<T> {
             }
         };
 
-        let attrs = SetFileAttributes {
-            mode: op.mode(),
-            size: op.size(),
-
-            uid: op.uid(),
-            gid: op.gid(),
-
-            atime: op.atime().and_then(to_duration),
-            mtime: op.mtime().and_then(to_duration),
-            ctime: op.ctime(),
-        };
+        let attrs = SetFileAttributes::builder()
+            .mode(op.mode())
+            .size(op.size())
+            .uid(op.uid())
+            .gid(op.gid())
+            .atime(op.atime().and_then(to_duration))
+            .mtime(op.mtime().and_then(to_duration))
+            .ctime(op.ctime())
+            .build();
 
         match self.fs.setattr(op.ino().into(), attrs) {
             Ok(obj) => {
                 let mut conv: reply::AttrOut = reply::AttrOut::default();
                 conv.attr().ino(op.ino());
-                conv.ttl(obj.ttl);
+                conv.ttl(obj.ttl());
 
                 self.copy_file_attr(&obj, op.ino().into(), conv.attr());
                 req.reply(conv).map_err(PolyfuseError::ReplyError)?;
@@ -556,17 +340,17 @@ impl<T: Filesystem> Runner<T> {
     fn copy_file_attr(&self, from: &FileAttributes, inode: INode, to: &mut reply::FileAttr) {
         to.ino(inode.to_u64());
 
-        to.mode(from.mode);
-        to.size(from.size);
-        to.nlink(from.nlink);
-        to.uid(from.uid);
-        to.gid(from.gid);
-        to.rdev(from.rdev);
-        to.blksize(from.blksize);
-        to.blocks(from.blocks);
-        to.atime(from.atime);
-        to.mtime(from.mtime);
-        to.ctime(from.ctime);
+        to.mode(from.mode());
+        to.size(from.size());
+        to.nlink(from.nlink());
+        to.uid(from.uid());
+        to.gid(from.gid());
+        to.rdev(from.rdev());
+        to.blksize(from.blksize());
+        to.blocks(from.blocks());
+        to.atime(from.atime());
+        to.mtime(from.mtime());
+        to.ctime(from.ctime());
     }
 }
 
